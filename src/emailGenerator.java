2e196c99821f6d1d37a4d4cb8ea5c9dfe64f5e75
gabogara <gabo1gara@gmail.com>
2026-08-13 00:21:48 -0700

    feat: add basic input console behavior

--- src/emailGenerator.java
+++ src/emailGenerator.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class emailGenerator {
     static String createEmail(String firstName,String lastName,String company,String domain){
         var fullName = String.join(".", firstName, lastName);
@@ -5,11 +7,18 @@ public class emailGenerator {
         return result;
     }
     public static void main(String[] args) {
-        String fName  = "Gabriel";
-        String lName = "Restrepo";
+        var input = new Scanner(System.in);
+        System.out.println("Write your first name: ");
+        String fName = input.nextLine();
+        //String fName  = "Gabriel";
+        System.out.println("Your first name is:" + fName);
+        System.out.println("Enter your lastname");
+
+        String lName = input.nextLine();
+        System.out.println("Your last name is: "+ lName);
         String comp = "intuit";
         String dom = ".com";
         var result_function = createEmail(fName,lName,comp,dom);
-        System.out.println(result_function);
+        System.out.println("Your New email is: "+result_function);
     }
 }
