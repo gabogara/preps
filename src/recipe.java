ecb5c6ab69d935a05b8685417092594b423dac70
gabogara <gabo1gara@gmail.com>
2026-08-18 00:36:20 -0700

    Finishing basic recipe class

--- src/recipe.java
+++ src/recipe.java
@@ -10,5 +10,16 @@ public class recipe {
         System.out.println("Enter ingredients: ");
         String recipeIng = input.nextLine();
 
+        System.out.println("Enter prep time: ");
+        int cookTime = Integer.parseInt(input.nextLine());
+
+        System.out.println("Enter the difficulty: ");
+        String difficulty = input.nextLine();
+
+
+        System.out.println("The recipe is: " + recipeName);
+        System.out.println("Ingredients are: " + recipeIng);
+        System.out.println("Cook Time is: " + cookTime);
+        System.out.println("Difficulty: " + difficulty);
     }
 }
