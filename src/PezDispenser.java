932d3c9f1800aeb7cadb8d89a08b283da41185f6
gabogara <gabo1gara@gmail.com>
2026-09-02 19:51:33 -0700

    add class PEZ exercise

--- src/PezDispenser.java
+++ src/PezDispenser.java
@@ -1,4 +1,43 @@
-public class PezDispenser {
+class PezDispenser {
 
-    String characterName = "Vegueta";
+    private final String characterName;
+    public static final int MAX_PEZ = 12;
+    private int pezCount;
+
+    public PezDispenser(String characterName){
+        this.characterName = characterName;
+        pezCount = 0;
+    }
+
+    public void fill(){
+         fill(MAX_PEZ);
+    }
+    public String getCharacterName(){
+        return characterName;
+    }
+
+    public int getPezCount(){
+        return pezCount;
+    }
+
+    public boolean isEmpty(){
+        return pezCount == 0;
+    }
+
+    public boolean dispense(){
+        boolean wasDispensed = false;
+        if(!isEmpty()){
+            pezCount--;
+            wasDispensed = true;
+        }
+        return wasDispensed;
+    }
+
+    public void fill(int pezAmount){
+        int newAmount = pezAmount+ pezCount;
+        if(newAmount > MAX_PEZ){
+            throw new IllegalArgumentException("That's too many PEZ to insert!!!");
+        }
+        pezCount = newAmount;
+    }
 }
--- src/usingClass.java
+++ src/usingClass.java
@@ -1,7 +1,45 @@
+import java.util.Scanner;
+
 public class usingClass {
     public static void main(String[] args) {
         System.out.println("******* Exploring Class behavior **********");
-        PezDispenser dispenser = new PezDispenser();
-        System.out.println("The instance of dispenser is: " + dispenser.characterName);
+        Scanner input = new Scanner(System.in);
+        PezDispenser dispenser = new PezDispenser(" PEZ Spiderman");
+
+        System.out.println("The instance of dispenser is: " + dispenser.getCharacterName());
+        System.out.println("The amount candies in the PEZ dispenser initial is: " + dispenser.getPezCount());
+        if(dispenser.isEmpty()){
+            System.out.println("Do you want to fill the PEZ dispenser?");
+            String answerFill = input.nextLine();
+            if(answerFill.equalsIgnoreCase("yes")){
+                System.out.println("Filling the dispenser with delicious PEZ...");
+                dispenser.fill();
+                System.out.printf("Now the dispenser now has: %d delicious PEZ%n", dispenser.getPezCount());
+
+                System.out.println("Do you want a eat all candies?: ");
+                String guilty = input.nextLine();
+                if(guilty.equalsIgnoreCase("yes")){
+                    while(dispenser.dispense()){
+                        System.out.println("OM NOM NOM!");
+                    }
+                    System.out.println("Do you want to fill more PEZ in the dispenser? Yes / No");
+                    String refill = input.nextLine();
+                    if(refill.equalsIgnoreCase("yes")){
+                        System.out.println("How many PEZ do you want to refill?");
+                        int amount = Integer.parseInt(input.nextLine());
+                        try{
+                            dispenser.fill(amount);
+                            System.out.printf("Perfect! Now you have: %d PEZ available!!!%n", dispenser.getPezCount());
+                        }catch(IllegalArgumentException iae){
+                            System.out.println("Whoa There!");
+                            System.out.printf("The error was: %s%n", iae.getMessage());
+                        }
+                        System.out.println("The program has finished!!!");
+                    }
+                }
+            }else{
+                System.out.println("Is ok, go back soon!...");
+            }
+        }
     }
 }
