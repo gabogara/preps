ac62daa3b7e1bef498eb8f43b5250b16e679dc23
gabogara <gabo1gara@gmail.com>
2026-09-13 00:15:25 -0700

    add exercises to practice list

---
+++ src/Listas.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Listas {
+    public static void main(String[] args) {
+        List<String>  myList = new ArrayList<>();
+        myList.add("Monday");
+        myList.add("Tuesday");
+        myList.add("Wednesday");
+        myList.add("Thursday");
+        myList.add("Friday");
+
+        for( String element: myList){
+            System.out.println(element);
+        }
+
+        System.out.println("Implementing Foreach:");
+//        myList.forEach( element -> {
+//            System.out.println("Element:" + element);
+//        });
+        myList.forEach(System.out::println);
+
+
+        //Other way to create list
+        List<String> names = Arrays.asList("Joshua", "Ivon", "Luis");
+    }
+}
---
+++ src/filesHandlers/CreateFiles.java
@@ -0,0 +1,8 @@
+package filesHandlers;
+
+public class CreateFiles {
+    public static void main(String[] args) {
+        String nameFile = "my_file.txt";
+
+    }
+}
